c4fda1337729bf53e0863936ce3e09b65c9c9fff
brookeaevans919 <brev5894@colorado.edu>
2025-10-03 12:46:04 -0600

    Update lasagna.py

--- exercises/concept/guidos-gorgeous-lasagna/lasagna.py
+++ exercises/concept/guidos-gorgeous-lasagna/lasagna.py
@@ -9,10 +9,12 @@ of a module and its functions and/or classes.
 
 
 #TODO: define your EXPECTED_BAKE_TIME (required) and PREPARATION_TIME (optional) constants below.
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
 
 
 #TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+def bake_time_remaining(elapsed_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -23,7 +25,8 @@ def bake_time_remaining():
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    pass
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
 
 
 #TODO: Define the 'preparation_time_in_minutes()' function below.
@@ -31,10 +34,25 @@ def bake_time_remaining():
 # You can do that on the line below the 'EXPECTED_BAKE_TIME' constant.
 # This will make it easier to do calculations, and make changes to your code.
 
+def preparation_time_in_minutes(layers):
+    """Calculate the preparation time.
+
+    :param layers: int - the number of layers of lasagna.
+    :return: int - total preparation time (in minutes) based on `PREPARATION_TIME`.
+    """
+    return layers * PREPARATION_TIME
 
 
 #TODO: define the 'elapsed_time_in_minutes()' function below.
 
+def elapsed_time_in_minutes(layers, elapsed_bake_time):
+    """Calculate the elapsed cooking time.
+
+    :param layers: int - the number of layers of lasagna.
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - total elapsed cooking time (in minutes).
+    """
+    return preparation_time_in_minutes(layers) + elapsed_bake_time
 
 
 # TODO: Remember to go back and add docstrings to all your functions
